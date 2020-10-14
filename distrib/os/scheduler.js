7a07e6f565607f1743e1b902194657467108bfd3
Alex Badia <badia.alex.h@gmail.com>
2020-10-14 04:05:03 -0400

    Fixed single process not executing in runall command. AxiOS now can run 1-3 programs round robin style using a quantum of 1... Other quantums must be tested, quantum of 1 should really test the context switching, now it's just a matter of what happens if a process finsihed before it's quantum...

--- distrib/os/scheduler.js
+++ distrib/os/scheduler.js
@@ -10,6 +10,21 @@
  *      Having a Ready Queue and Resident List should make calculating the AWT much easier
  *      later... *Cough* *Cough* time spent in the ready queue *Cough* *Cough*
  *
+ * Why Did I Implement them all?
+ *
+ *      If you noticed when I (redundantly) implemeneted the Segmentation algorithms: First-Fit, Worst-Fit, Best-Fit
+ *      I aced it on the midterm (not like it was suppose to be hard, these are very simple concepts). BUT...
+ *
+ *      I learn best through "doing". The only reason why I still remember half of Gormanly's and Schwartz's information
+ *      from Org and Arch and Database is well, I remember explaining all the concepts to a friend of mine (Brian, who will
+ *      probably have you next semester). Stuff with the Arduinos (rolling averages), Atomicity and all that jazz.
+ *
+ *      Otherwise, teaching me theory from a book is the equivalent to throwing a bunch of mud at a wall and seeing what sticks
+ *      (I know, I'm not the sharpest tool in the shed). This is why I'm glad we're actually coding this...
+ *
+ *      So yeah, I implemented them all (again not like this is suppose to be a "hard" class, this is an introductory class to OS's anyway
+ *         and like with everything else we're only scraping the surface).
+ *
  */
 var TSOS;
 (function (TSOS) {
@@ -45,12 +60,15 @@ var TSOS;
             ///
             /// Quantum expires when the CPU Burst is the: (Starting CPU Burst + Quantum)
             else if ((this.relativeStartingBurst + this.quantum) === _CPU_BURST) {
-                /// Grab the another process from the Ready Queue
-                this.currentProcessControlBlock = _Scheduler.readyQueue.shift();
-                /// Attach the process to the CPU
-                _Dispatcher.attachNewPcbToCPU();
-                /// Before we begin CPU execution, Update Relative Starting Burst
-                this.relativeStartingBurst = _CPU_BURST;
+                /// Check if there is another process to grab from the Ready Queue
+                if (this.readyQueue.length > 0) {
+                    /// Grab the another process from the Ready Queue
+                    this.currentProcessControlBlock = _Scheduler.readyQueue.shift();
+                    /// Attach the process to the CPU
+                    _Dispatcher.attachNewPcbToCPU();
+                    /// Before we begin CPU execution, Update Relative Starting Burst
+                    this.relativeStartingBurst = _CPU_BURST;
+                } /// if
             } /// if
             /// Context Swich Complete Continue CPU Execution
             this.currentProcessControlBlock.processState = "Running";
